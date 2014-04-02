f4524962e9500f85ef71641fbf465aa810c9fa0b
Gambloide <sebastian.m.reuter@gmail.com>
2014-04-02 11:01:54 +0200

    Gave user choice of shapes to add

--- Editor/controllers/contentController.js
+++ Editor/controllers/contentController.js
@@ -19,24 +19,113 @@ app.controller("contentController", function ($scope, $timeout, presentationFact
         });
     };
 
-    $scope.addItem = function () {
-        $scope.presentation.slides[0].items.push({
-            id: Math.random().toString(36).slice(2),
-			location : [100, 100],
-			layer : 0, //(Layer.position)
-			height : 100,
-			width : 150,
-            rotation : 0.0,
-			related_items : [], //Item Array
-			shape : {},
-			text : {},
-			style : {
-				background : "#1abc9c",
-				border: '',
-				border_radius : 10
-			},
-			deleted: false
-		});
+    $scope.addItem = function (shape) {
+        alert('Yo, you choose a frickin\' ' + shape);
+        var shapes = {
+            'circle': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 130,
+                width : 130,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 100
+                },
+                deleted: false
+            },
+            'square': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 125,
+                width : 125,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 10
+                },
+                deleted: false
+            },
+            'rectangle': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 100,
+                width : 150,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 10
+                },
+                deleted: false
+            },
+            'heading': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 100,
+                width : 150,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 10
+                },
+                deleted: false
+            },
+            'textbox': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 100,
+                width : 150,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 10
+                },
+                deleted: false
+            },
+            'footer': {
+                id: Math.random().toString(36).slice(2),
+                location : [100, 100],
+                layer : 0, //(Layer.position)
+                height : 100,
+                width : 150,
+                rotation : 0.0,
+                related_items : [], //Item Array
+                shape : {},
+                text : {},
+                style : {
+                    background : "#1abc9c",
+                    border: '',
+                    border_radius : 10
+                },
+                deleted: false
+            },
+        };
+        $scope.presentation.slides[0].items.push(shapes[shape]);
     };
     
     $scope.addSlide = function () {
