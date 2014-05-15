972ace1904d5c7eed74082377f72135c383e0398
Gambloide <sebastian.m.reuter@gmail.com>
2014-05-15 21:00:13 +0200

    Augmented presenterMode with Angular magic as well

---
+++ Theater/controllers/presenterController.js
@@ -0,0 +1,10 @@
+app.controller("presenterController", function ($scope, $timeout) {
+	"use strict";
+    $scope.presentation = {};
+    
+    init();
+    
+    function init() {
+        //nothing here, yet
+    }
+});
